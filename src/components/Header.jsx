dad1cc132e5d9186469bf8c0d6e79dd5ce59140e
Lawrence Mindanao <mindanaolawrence86@gmail.com>
2025-10-07 19:17:06 +0800

    added mobile navbar burger menu for mobile responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,21 @@ function Header() {
     <nav className="navbar navbar-expand-lg custom-navbar">
       <div className="container">
         <Link className="navbar-brand" to="/">University of Cabuyao</Link>
-        <div>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button> 
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/mission-vision">Mission & Vision</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/gallery">Gallery</Link></li>
