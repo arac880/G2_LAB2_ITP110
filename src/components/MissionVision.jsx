74df848c66a851a5a6d4f72d4a1f69db0e10b3bc
Jake Abriam <jake.abriam@example.com>
2025-10-07 22:54:57 +0800

    Updated MissionVision component

--- src/components/MissionVision.jsx
+++ src/components/MissionVision.jsx
@@ -1,11 +1,17 @@
 function MissionVision() {
   return (
     <div className="container mt-4">
+      <h2>Core Values</h2>
+      <p>As God-fearing  institution respecting multi faith of people PnC adheres to the following core values 
+ P - Personal dignity 
+N - nurturing community 
+C - commitment to excellence.</p>
       <h2>Mission</h2>
-      <p>To provide excellent education for the holistic development of students.</p>
+      <p>As an-institution of higher learning, UC (PnC)  is committed to equip individual with knowledge, skills and values that will enable them to achieve their professionals goals and provide leadership and service for national development.</p>
       <h2>Vision</h2>
-      <p>Pamantasan ng Cabuyao as a premier institution of higher learning in the region.</p>
+      <p>A premier institution higher learning in Region IV - developing globally competitive and valued-laden professionals and leaders instrumental to community development and nation building.</p>
     </div>
+    
   );
 }
 export default MissionVision;
