80537888f0b8fe91989f0524c94d1aeab5f8ed5d
CeaZapata <zapatacarille13@gmail.com>
2025-10-07 15:52:41 +0800

    Adding images to the Gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,13 +4,37 @@ function Gallery() {
       <h2>Gallery</h2>
       <div className="row">
         <div className="col-md-4 mb-3">
-          <img src="https://via.placeholder.com/300" alt="PNC 1" className="img-fluid rounded" />
+          <img src="/src/assets/Images/PNC 1.png" alt="PNC 1" className="img-fluid rounded" />
         </div>
-        <div className="col-md-4 mb-3">
-          <img src="https://via.placeholder.com/300" alt="PNC 2" className="img-fluid rounded" />
+         <div className="col-md-4 mb-3">
+          <img src="/src/assets/Images/PNC 2.jpg" alt="PNC 2" className="img-fluid rounded" />
         </div>
-        <div className="col-md-4 mb-3">
-          <img src="https://via.placeholder.com/300" alt="PNC 3" className="img-fluid rounded" />
+         <div className="col-md-4 mb-3">
+          <img src="/src/assets/Images/PNC 3.jpg" alt="PNC 3" className="img-fluid rounded" />
+        </div>
+         <div className="col-md-4 mb-3">
+          <img src="/src/assets/Images/PNC 4.jpg" alt="PNC 4" className="img-fluid rounded" />
+        </div>
+         <div className="col-md-4 mb-3">
+          <img src="/src/assets/Images/PNC 5.jpg" alt="PNC 5" className="img-fluid rounded" />
+        </div>
+         <div className="col-md-4 mb-3">
+          <img src="/src/assets/Images/PNC 6.jpg" alt="PNC 6" className="img-fluid rounded" />
+        </div>
+         <div className="col-md-4 mb-3">
+          <img src="/src/assets/Images/PNC 7.png" alt="PNC 7" className="img-fluid rounded" />
+        </div>
+         <div className="col-md-4 mb-3">
+          <img src="/src/assets/Images/PNC 8.png" alt="PNC 8" className="img-fluid rounded" />
+        </div>
+         <div className="col-md-4 mb-3">
+          <img src="/src/assets/Images/PNC 9.png" alt="PNC 9" className="img-fluid rounded" />
+        </div>
+         <div className="col-md-4 mb-3">
+          <img src="/src/assets/Images/PNC 10.png" alt="PNC 10" className="img-fluid rounded" />
+        </div>
+         <div className="col-md-4 mb-3">
+          <img src="/src/assets/Images/PNC 11.png" alt="PNC 11" className="img-fluid rounded" />
         </div>
       </div>
     </div>
