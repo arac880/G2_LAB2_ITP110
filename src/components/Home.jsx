a760c10a3716a8ec63800ff2928b34c957f772ea
arac880 <ceresarachristina74@gmail.com>
2025-10-07 16:23:48 +0800

    update on Home jsx added section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,6 +14,18 @@ function Home() {
       }}
       >
       </div>
+
+      <section className="hometwo text-center py-5 bg-light">
+        <h1 className="fw-bold text-success">Pamantasan ng Cabuyao</h1>
+        <p className="lead text-secondary">
+          “Dangal ng Bayan, bringing pride and honor to the nation.”
+        </p>
+        <p>
+          Welcome to the official website of the University of Cabuyao — a center of excellence committed to developing future leaders and professionals guided by integrity, competence, and heroism.
+          The Pamantasan ng Cabuyao is a university in the city of Cabuyao, province of Laguna, Philippines. It was founded by then Mayor Proceso “Etok” D. Aguillo, through the enactment of Municipal Ordinance 2003-059 approved on April 16, 2003. TO OUR UNIVERSITY
+        </p>
+      </section>
+
     </>
     
   );
