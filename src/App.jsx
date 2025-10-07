83e8f987638768d609eaa2b881bfafb705eb58aa
arac880 <ceresarachristina74@gmail.com>
2025-10-07 16:13:32 +0800

    adding footer while using routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./components/Home";
 import MissionVision from "./components/MissionVision";
 import Gallery from "./components/Gallery";
 import About from "./components/About";
 import Contact from "./components/Contact";
+
 import "./App.css";
 
 function App() {
@@ -19,6 +21,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<h2 className="text-center mt-5">404 Page Not Found</h2>} />
       </Routes>
+      <Footer />
     </>
   );
 }
